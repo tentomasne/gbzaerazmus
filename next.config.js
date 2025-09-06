80bad2475b97c778d4086919396f1b0df60973ee
Tomáš <159623262+tentomasne@users.noreply.github.com>
2025-09-06 23:05:36 +0200

    sec

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
+const { withSecurityTxt } = require('@tentomasne/nextjs-security-txt');
+
+const securityTxtConfig = {
+  contact: 'mailto:gbza@davidik.eu',
+  expires: '2025-12-31T23:59:59Z',
+  preferredLanguages: ['en', 'sk', "cs", "fr"]
+};
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
-
-module.exports = nextConfig;
+module.exports = withSecurityTxt(securityTxtConfig)({
+  ...nextConfig,
+});
